fa0c08fece77480bbf8dfcfc1609c0b60d513977
gues-med <medgues7@gmail.com>
2022-09-11 14:40:06 +0100

    live version of theo website

---
+++ script.js
@@ -0,0 +1,5 @@
+dan = "https://miro.com/app/live-embed/uXjVPceX6UE=/?moveToViewport=-768,-394,1536,787&embedId=927557504986"
+generic = "https://miro.com/app/live-embed/uXjVPc4I_9w=/?moveToViewport=-768,-394,1536,787&embedId=558209453017"
+selectedUser = generic
+actualWhiteboard = selectedUser
+document.getElementById("whiteboard").src = actualWhiteboard
